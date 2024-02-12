d1905db3f81df9f6cf9094ca55c9010702a37d63
MSelmanSavas <mehmetselmansavas@gmail.com>
2024-02-12 21:06:34 +0300

    Started adding materials and rendering components of entities

--- Assets/Scripts/ECS/Components/Player/PlayerControllerComponent.cs
+++ Assets/Scripts/ECS/Components/Player/PlayerControllerComponent.cs
@@ -1,6 +1,17 @@
+using System;
 using Unity.Entities;
 
-public class PlayerControllerComponent : IComponentData
+public class PlayerControllerComponent : IComponentData, IDisposable, ICloneable
 {
     public PlayerController PlayerController;
+
+    public object Clone()
+    {
+        return new PlayerControllerComponent { PlayerController = UnityEngine.Object.Instantiate(PlayerController) };
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(PlayerController.gameObject);
+    }
 }
